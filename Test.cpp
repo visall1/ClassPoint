dd015ea9f644cb91f52ef2b090575bcf2b3d4581
visall1echo <khmercr12@gmail.com>
2024-06-29 20:26:48 +0700

    second commit

--- Test.cpp
+++ Test.cpp
@@ -1,43 +1,44 @@
-#include<iostream>
+#include <iostream>
 #include<cmath>
 using namespace std;
-class Point {
-	double x, y;
+
+class Point
+{
+    double x, y;
 public:
-	double e = 0.0001;
-	static string name; //static member variable
-	Point() :x(0), y(0) {
-		Point::name = name;
-	}
-	Point(double x, double y, string name)
-		:x(x), y(y) {
-		Point::name = name;
-	}
-	void Show() {
-		cout << name << " : X= " << x << " Y= " << y << endl;
-	}
-	bool IsSame(const Point& p1) {
-		double ex = abs(x = p1.x);
-		double ey = abs(y = p1.y);
-		return ex < e && ey < e;
-	}
+    static double e;
+    static string name;//static member variable
+    Point() :x(0), y(0) {
+        Point::name = "";
+    }
+    Point(double x, double y, string name)
+        :x(x), y(y) {
+        Point::name = name;
+    }
+    void Show() {
+        cout << name << ": x=" << x << ", y=" << y << endl;
+    }
+    bool IsSame(const Point& p2) {
+        double ex = abs(x - p2.x);
+        double ey = abs(y - p2.y);
+        return ex < e && ey < e;
+    }
 };
 string Point::name = "";
+double Point::e = 0.0001;
+int main()
+{
+    Point p1(5.000005, 9.00009, "");
+    Point p2(5.000035, 9.000045, "ABC");
+    Point::e = 0.001;
+    Point::name = "Point";
+    std::cout << "Hello\n";
+    p1.Show();
+    p2.Show();
+    if (p1.IsSame(p2)) { cout << "p1 == p2\n"; }
+    else cout << "p1 != p2\n";
 
-int main() {
-
-	Point p1(5.000005, 9.00000, "");
-	Point p2(5.000035, 9.000045, "ABC");
-	p1.e = 0.00001;
-	Point::name = "Point";
-	std::cout << "Hello\n";
-	p1.Show();
-	p2.Show();
-
-	if (p1.IsSame(p2)) { cout << "p1 ==p2\n"; }
-	else cout << "p1 != p2 \n";
-
-	if (p2.IsSame(p1)) { cout << "p2 == p1\n"; }
-	else cout << "p2 != p2\n";
-	return 0;
+    if (p2.IsSame(p1)) { cout << "p2 == p1\n"; }
+    else cout << "p2 != p1\n";
+    return 0;
 }
